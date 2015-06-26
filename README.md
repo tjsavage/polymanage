622ebc871befc9f6900b486ce1ff70b9eebbddb0
Taylor Savage <tjsavage@google.com>
2015-06-25 19:41:53 -0700

    added the ability to list labels for repo issues

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # polymanage
 A management tool for teams using Polymer and building lots of elements
+
+## Features
+
+Implemented
+* List repos based on regexes
+
+Planned
+* List tags for repos
--- src/githubManager.js
+++ src/githubManager.js
@@ -14,6 +14,8 @@ var github = new GithubAPI({
 var config = require('../config.json');
 var Promise = global.Promise || require('es6-promise').Promise;
 
+var clic = require('cli-color');
+
 /*
 * @param {Array<String>} repos A list of strings representing repositories to
 *   act on. Strings can be in the form Organization/repo or regex in the form
@@ -24,19 +26,77 @@ var GithubManager = function GithubManager(repos) {
     type: "oauth",
     token: config.github.token
   });
-  this._repos = repos;
+
+  this._repoStrings = repos;
+  this._reposPromise = GithubManager._retrieveRepos(this._repoStrings);
 }
 
 GithubManager.prototype = {
-  list: function list() {
-    GithubManager._retrieveRepos(this._repos).then(function(repos) {
+  list: function list(properties, verbose) {
+    this._reposPromise.then(function(repos) {
       for(var i = 0; i < repos.length; i++) {
-        console.log(repos[i].name);
+        if(verbose) {
+          console.log(repos[i]);
+        } else {
+          console.log(repos[i].name);
+        }
+      }
+    });
+  },
+
+  labels: function labels(list, group) {
+    this._reposPromise.then(GithubManager._retrieveLabelsFromRepos).then(function(repos) {
+      if (list) {
+        for(var i = 0; i < repos.length; i++) {
+          if (group) console.log(repos[i].name);
+
+          for(var j = 0; j < repos[i].labels.length; j++) {
+            var labelsStr = ""
+            if (group) labelsStr += "\t";
+
+            labelsStr += repos[i].labels[j].name;
+
+            console.log(labelsStr);
+          }
+        }
       }
     });
   }
 }
 
+/*
+* @param {Array<Repos>} repos A list of repo objects to retrieve tags for
+* @return {Promise<Array<Repos>>} A promise that resolves to a list of repo
+*   objects, with the list of tags added as a top-level property
+*/
+GithubManager._retrieveLabelsFromRepos = function(repos) {
+  var labelPromises = [];
+
+  for (var i = 0; i < repos.length; i++) {
+
+    var labelPromise = new Promise(function(resolve, reject) {
+      var repo = repos[i];
+      github.issues.getLabels({
+        user: repo.owner.login,
+        repo: repo.name,
+        //TODO: Apply this recursively to get all pages no matter what.
+        per_page: 100
+      }, function(err, labels) {
+        if (err) reject(err);
+
+        repo["labels"] = labels;
+        resolve(repo);
+      });
+    })
+
+    labelPromises.push(labelPromise);
+  }
+
+  return Promise.all(labelPromises).then(function(repos) {
+    console.log(repos[0].name, repos[1].name)
+    return repos;
+  });
+}
 /*
 * @param {Array<String>} repoStrings A list of strings to resolve to repos
 * @return {Promise} Returns a promise that resolves to a list of repo objects
